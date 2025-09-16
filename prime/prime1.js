fcfca98d4d7bd9d53eef824fa090dacbbc7b1117
Varnwal Vivek <varnwalvivek4644@gmail.com>
2025-09-16 11:45:15 +0530

    prime

--- prime/prime1.js
+++ prime/prime1.js
@@ -2,24 +2,21 @@ var prime = prompt("Enter the input");
 
 
 var num = parseInt(prime);
-var count = 0;
 
-for (let j = 1; j <= num; j++) //100
+for (let j = 2; j <= num; j++) {
+    var count = 0;
+
+    for (let i = 1; i <= j; i++)
     {
-        for (let i = 1; i <= j; i++) 
-            {
-            if (j % i == 0) 
-                {
-                    count++;
-                }
-            }
+        if (j % i == 0) {
+            count++;
+        }
+    
     }
-
-
-
-if (count == 2) {
-    console.log("it is prime number " + num);
+    if (count == 2) {
+        console.log("it is prime number " + j);        
+    }
 }
-else {
-    console.log("it is not prime number " + num);
-}
+
+
+
